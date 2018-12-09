93a827ea091dffd39615515d38ff37d32feacecb
Unknown <a4ramy@hotmail.com>
2018-12-09 21:15:47 +0100

    Adding author and repo link to the pedal.

--- src/app/pages/plugin-details/plugin-details.component.ts
+++ src/app/pages/plugin-details/plugin-details.component.ts
@@ -14,11 +14,17 @@ export class PluginDetailsComponent {
     id: string;
     previewUrl: string;
     types: string[];
+    description: string;
+    author: string;
+    repoLink: string;
 
     constructor(private activatedRoute: ActivatedRoute) {
         this.id = this.activatedRoute.snapshot.params['id'];
         this.previewUrl = 'https://picsum.photos/400';
         this.types = ['Instrument', 'Guitare'];
+        this.description = 'This is an awsome plugin with a very good graphical interface. \n It can add many effects to the sound.';
+        this.author = 'Amine';
+        this.repoLink = 'https://github.com/falkTX/FluidPlug';
     }
 
 }
